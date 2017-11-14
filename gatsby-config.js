9c24b33e61ec620b7251d81fd4c10785f8edd5fd
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2017-11-14 16:24:29 +0800

    added sitemap and netlify header

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,10 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-styled-components`]
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-sitemap`
+  ]
 }
