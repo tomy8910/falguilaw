8a08aa683b6efc6af97cac963ae91cf2e74fb734
Tomas Alejandro V. Falgui III <tomyfalguiiii@yahoo.com>
2017-11-14 16:49:38 +0800

    added siteurl

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`
+    title: `Falgui Law Offices`,
+    siteUrl: 'https://www.explorer-vivian-34063.com/'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
